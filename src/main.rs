24f54984daa90ecfd7841e0a9c39082aaa4c4d0f
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-07-29 12:32:24 -0500

    Reading Config and arguments from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+
+use huffcomp::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = huffcomp::run(config) {
+        eprint!("Application error: {}", e);
+        process::exit(1);
+    }
 }
